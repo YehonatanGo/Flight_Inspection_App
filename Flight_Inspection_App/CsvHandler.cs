85462da774a2a63d3538cbeccaf7454170d3d624
Neriya-Fisch <72704008+Neriya-Fisch@users.noreply.github.com>
2021-03-31 16:48:21 +0300

    CsvHandler

---
+++ Flight_Inspection_App/CsvHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Flight_Inspection_App
+{
+    class CsvHandler
+    {
+        private string path;
+        private int row_num;
+        private List<string> my_list = new List<string>();
+
+        public CsvHandler(string path)
+        {
+            this.path = path;
+            parseCsv();
+        }
+
+        private void parseCsv()
+        {
+            using (var reader = new StreamReader(path))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (line == null) continue;
+                    my_list.Add(line);
+                }
+                row_num++;
+            }
+        }
+    }
+}
