4b4a7bf4a55a423c930a97078e4bdb1cbcfc7bdb
ItamarFi <itamar.fisch@gmail.com>
2021-04-09 17:41:38 +0300

    graphs: doesnt work. yet

---
+++ Flight_Inspection_App/controls/graphs.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Flight_Inspection_App.controls
+{
+    /// <summary>
+    /// Interaction logic for graphs.xaml
+    /// </summary>
+    public partial class graphs : UserControl
+    {
+        public graphs()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            vm.VM_DisplayedFeature = ListBox_Features_List.SelectedItem.ToString();
+        }
+    }
+}
