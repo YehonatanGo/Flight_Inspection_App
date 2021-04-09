7705b4e5909c8ae2bd85e19e9d377c808821ae26
Yehonatan <yehonatan.goldfarb@gmail.com>
2021-04-09 12:43:04 +0300

    Edited plots a little

--- Flight_Inspection_App/CsvHandler.cs
+++ Flight_Inspection_App/CsvHandler.cs
@@ -123,7 +123,7 @@ namespace Flight_Inspection_App
 
         public float getFeatureByLine(string feature, int line)
         {
-            if(feature == null)
+            if(feature == null || feature.Equals("none"))
             {
                 return 0;
             }
